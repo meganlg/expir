5e0ded5a9c27ca2db74e1658bdb92a6e9f68fe5c
meganlg <meganleagillen@gmail.com>
2020-04-20 15:50:05 -0500

    update testerish and pycheatsheet

--- pycheatsheet.md
+++ pycheatsheet.md
@@ -28,4 +28,40 @@ expression | combination of values, operators, and values that represents a sing
 concatenate | join two operands end-to-end
 comment | info/text in a program that has no effect on execution on
 
-#updated for binder section 2
+#updated for binder section 2 ^
+
+
+
+code | type
+---|---
+"Hello World" |	str
+20 | int
+20.5 | float
+ 1j	| complex
+ ["apple", "banana", "cherry"] |	list
+ ("apple", "banana", "cherry")	| tuple
+ range(6)	| range
+ {"name" : "John", "age" : 36}	| dict
+ {"apple", "banana", "cherry"}	| set
+ frozenset({"apple", "banana", "cherry"})	| frozenset
+ True |	bool
+ b"Hello"	| bytes
+ bytearray(5)	| bytearray
+ memoryview(bytes(5))	| memoryview
+
+
+
+### lists 
+ *lists* method | Description
+ ---|---
+ append() | Adds an element at the end of the list
+ clear() |	Removes all the elements from the list
+ copy() |	Returns a copy of the list
+ count() |	Returns the number of elements with the specified value
+ extend()	| Add the elements of a list (or any iterable), to the end of the current list
+ index() | Returns the index of the first element with the specified value
+ insert()	| Adds an element at the specified position
+ pop() | Removes the element at the specified position
+ remove()	| Removes the item with the specified value
+ reverse() |	Reverses the order of the list
+ sort() |	Sorts the list
--- testerish.py
+++ testerish.py
@@ -10,6 +10,134 @@ print(n)
 
 #change to test branch "test"
 
+
 def function1():
     print_words('whateverr')
-def function1()
+
+
+#strings and Lists
+
+#.find for strings, .index for lists
+
+age = '16'
+name = 'megan'
+
+list= [name, age]
+':'.join(list)
+
+
+#booleans
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+first_name = 'megan'
+last_name = 'gillen'
+
+# Join the variables with a '_'
+full_name = first_name + last_name
+print(full_name)
+
+
+
+# Capitalize first letters
+full_name.title()
+
+age = 16
+
+
+
+# Lists
+
+''' # Create a list that contains the `full_name` and `age` variables
+my_list = [full_name, age]
+
+# print the index for `age`
+print(my_list.index(age))
+
+seclist = ['bob', 20]
+my_list.append(seclist)
+print(my_list)
+
+my_list.extend(seclist)
+print(my_list)
+
+del my_list[2:]
+print(my_list) '''
+
+
+#tuples
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+#turn lists (or other type?) into tuple
+print(tuple(my_list))
+
+#in keyword
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+
+
+
+#for loop
+fruits = ["apple", "banana", "cherry"]
+for t in fruits:
+  print(t)
+
+for x in "banana":
+  print(x)
+
+#sets
+aset = {"the", "cat"}
+print("the" in aset)
+
+for x in aset:
+    print(x)
+
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+
+set3 = set1.union(set2)
+print(set3)
+#or
+set1.update(set2)
+print(set1)
+
+
+#dictionaries
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+x = thisdict["model"]
+
+thisdict["year"] = 2018
+print(thisdict)
+
+
+#nested dictionary
+
+nestdict = {
+    "first" : {"one":"uno" , "two":"dos",},
+    "second" : {"three":"tres" , "four":"cuatro"}
+}
+
+nestdict['first']['two']
+
+nestdict['second']['five'] = ['cinco']
+print(nestdict)
+nestdict['second']
+nestdict['second']['four']
